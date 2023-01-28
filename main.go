47acd3948cb136777656ecece024f18e9389d251
alex <alexnlarsen@gmail.com>
2023-01-28 16:35:40 +0100

    Add event masks and different dispatcher modes

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
-	"log"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -12,8 +13,9 @@ import (
 )
 
 var Command string
+var options watcher.Options
 
-func OnChange(op fsnotify.Op, path string) {
+func OnChange(op fsnotify.Op, path string, context context.Context) {
 	if Command == "" {
 		return
 	}
@@ -24,31 +26,156 @@ func OnChange(op fsnotify.Op, path string) {
 	c = strings.ReplaceAll(c, "%e", opname)
 
 	cmd := exec.Command("bash", "-c", c)
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		log.Printf("Error running command %v: %v\n", c, err)
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+	select {
+	case <-context.Done():
+		return
+	default:
+		err := cmd.Start()
+		if err != nil {
+			fmt.Printf("Error running command %v: %v\n", c, err)
+			return
+		}
+		if options.Mode == watcher.Kill {
+			go func() {
+				<-context.Done()
+				if err := cmd.Process.Kill(); err != nil {
+					text := err.Error()
+					if strings.Contains(text, "process already finished") {
+						return
+					}
+					fmt.Printf("failed to kill process: %v", text)
+				}
+			}()
+		}
+		cmd.Wait()
+		return
 	}
-	fmt.Print(string(out))
+}
+
+func parseEventMask(mask string) (op fsnotify.Op, err error) {
+	parts := strings.Split(mask, "|")
+	for _, part := range parts {
+		p := strings.TrimSpace(part)
+		p = strings.ToLower(p)
+
+		if p == "create" {
+			op |= fsnotify.Create
+		} else if p == "write" {
+			op |= fsnotify.Write
+		} else if p == "remove" {
+			op |= fsnotify.Remove
+		} else if p == "rename" {
+			op |= fsnotify.Rename
+		} else if p == "chmod" {
+			op |= fsnotify.Chmod
+		} else {
+			err = fmt.Errorf("No such event '%v'", part)
+			return
+		}
+	}
+	return op, err
+}
+
+func usage() {
+	fmt.Printf("Usage: gwatch [OPTIONS]... [PATHS]...\n")
+	deferred := make([]string, 0)
+	flag.VisitAll(func(f *flag.Flag) {
+		if f.Name == "help" {
+			return
+		}
+		typeName, usage := flag.UnquoteUsage(f)
+		lines := strings.Split(usage, "\n")
+		usage = strings.Join(lines, "\n\t")
+		defaultValue := f.DefValue
+		if typeName == "string" {
+			defaultValue = fmt.Sprintf(`"%v"`, defaultValue)
+		}
+		if typeName == "" { // bool
+			// Show boolean flags last
+			deferred = append(deferred, fmt.Sprintf("  -%v\n\t%v\n", f.Name, usage))
+		} else {
+			fmt.Printf("  -%v %v (default %v)\n\t%v\n", f.Name, typeName, defaultValue, usage)
+		}
+	})
+	for _, def := range deferred {
+		fmt.Print(def)
+	}
+	fmt.Print(`  -help
+        Show this help text
+`)
+	fmt.Print(`  PATHS strings (default ".")
+        Any number of paths can be watched. 
+        Watching a directory will cause all subdirectories to be watched recursively.
+        Creating new subdirectories in a watched directory will automatically add the new directories to the watch list.
+        When files are added and removed from watched directories, they are also automatically added and removed from the watch list.
+
+        If a file is watched, events will only be generated for that file. In other words, if the file is deleted,
+        and a new file is created with the same name, events will not be generated for the new file.
+        For the same reason, it is not possible to watch a file which does not yet exist.
+        This restriction does not apply if instead the directory containing the file is being watched.`)
 }
 
 func main() {
-	// TODO: Add options
-	// Grace period -- how often should we propagate events
-	// Block/Allowlist of eventtypes
-	// 1. "I only care about WRITE"
-	// 2. "I don't care about CHMOD"
-	var help = flag.Bool("help", false, "Show help")
+	var help = flag.Bool("help", false, "Show this help message")
 	var includeHidden = flag.Bool("include-hidden", false, "Recurse into hidden directories")
+	var queueSize = flag.Int("queue-size", 1, "The maximum number of queued events. Old events will be evicted.")
 
-	log.SetFlags(0)
-	flag.StringVar(&Command, "command", "", "The command to run when the file changes")
+	var mode string
+	flag.StringVar(&mode, "mode", "Concurrent", `Event backlog behavior.
+Concurrent: 
+    Run the command concurrently whenever an event is fired, even if previous handlers has not yet finished 
+Queue: 
+    Queue events and run the handler in sequence 
+Block: 
+    Ignore events while the handler is running, and resume listening to events when it finishes. 
+    This is a special case of 'Queue' with a queue size of 0
+Kill: 
+    Stop the currently running command when a new event arrives, and run the new one`)
+	var eventString string
+
+	flag.StringVar(&eventString, "eventMask", "Create|Write|Remove|Rename|Chmod", "Mask of events to watch.")
+
+	flag.StringVar(&Command, "command", "echo %e %f",
+		`The command to run when the file changes.
+The command is invoked like "bash -c '<command>'"
+Simple string replacement is supported to identify what happened:
+  %e: The mask of events that was triggered (e.g. CHMOD|WRITE is possible)
+  %f: Relative path to the file
+Shell control characters are quoted when macros are expanded, so a file named a&b will expand to a\&b etc.
+No escaping is done on the provided command.`)
+
+	flag.Usage = usage
+
 	flag.Parse()
 	items := flag.Args()
 
-	if *help || len(items) == 0 || Command == "" {
+	if len(items) == 0 {
+		items = append(items, ".")
+	}
+
+	if *help || Command == "" {
 		flag.Usage()
 		return
 	}
 
-	watcher.WatchItems(items, *includeHidden, OnChange)
+	m, err := watcher.GetMode(mode)
+	if err != nil {
+		panic(err)
+	}
+
+	eventMask, err := parseEventMask(eventString)
+	if err != nil {
+		panic(err)
+	}
+
+	options = watcher.Options{
+		QueueSize:     *queueSize,
+		IncludeHidden: *includeHidden,
+		Mode:          m,
+		EventMask:     eventMask,
+		Handler:       OnChange,
+	}
+	watcher.WatchItems(items, &options)
 }
--- pkg/watcher/watcher.go
+++ pkg/watcher/watcher.go
@@ -1,26 +1,90 @@
 package watcher
 
 import (
+	"context"
+	"fmt"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
 	"strings"
+	"sync"
 	"time"
 
 	"github.com/fsnotify/fsnotify"
 )
 
+var (
+	// The handler is run concurrently when new events occur.
+	Concurrent Mode = Mode{mode: 0}
+	// Events are queued, and the handler is invoked in the order the events occur.
+	// A slow handler will cause events to fill indefinitely, unless a queue size is set.
+	Queue Mode = Mode{mode: 1}
+	// New events are blocked until the current handler has finished.
+	Block Mode = Mode{mode: 2}
+	// The context for the current handler is cancelled, and a new event is fired.
+	// If the handler doesn't properly handle the cancel event, this has the same behavior
+	// as a concurrent.
+	Kill Mode = Mode{mode: 3}
+)
+
+type Mode struct {
+	mode int
+}
+
+func (m Mode) ToString() string {
+	if m == Concurrent {
+		return "Concurrent"
+	}
+	if m == Queue {
+		return "Queue"
+	}
+	if m == Block {
+		return "Block"
+	}
+	if m == Kill {
+		return "Kill"
+	}
+	return "Error"
+}
+
+func GetMode(m string) (mode Mode, err error) {
+	if m == Concurrent.ToString() {
+		mode = Concurrent
+	} else if m == Queue.ToString() {
+		mode = Queue
+	} else if m == Block.ToString() {
+		mode = Block
+	} else if m == Kill.ToString() {
+		mode = Kill
+	} else {
+		err = fmt.Errorf("No such mode '%v'", m)
+	}
+	return mode, err
+}
+
+type Options struct {
+	// Only valid if Mode is Queue
+	QueueSize int
+	// Set the behavior when multiple events occur before the callback has finished
+	Mode Mode
+	// Whether or not to watch hidden files and directories
+	IncludeHidden bool
+	// The function which is invoked when an event occurs
+	Handler func(op fsnotify.Op, path string, ctx context.Context)
+	// Mask of events to watch
+	EventMask fsnotify.Op
+}
+
 func (w *WatchedDir) walkAndAddAll(root string, emit bool) {
 	if isDir(root) {
 		filepath.WalkDir(root, func(path string, d fs.DirEntry, err error) error {
 			if d.IsDir() {
-				if w.includeHidden == false && isHidden(d.Name()) {
+				if w.options.IncludeHidden == false && isHidden(d.Name()) {
 					return nil
 				}
 				err := w.watcher.Add(path)
 				if err != nil {
-					log.Printf("Error adding watch: %v", err.Error())
+					fmt.Printf("Error adding watch: %v", err.Error())
 				}
 			}
 
@@ -33,13 +97,13 @@ func (w *WatchedDir) walkAndAddAll(root string, emit bool) {
 	} else {
 		err := w.watcher.Add(root)
 		if err != nil {
-			log.Printf("Error adding watch: %v", err.Error())
+			fmt.Printf("Error adding watch: %v", err.Error())
 		}
 	}
 }
 
-func Create(paths []string) *WatchedDir {
-	var w = WatchedDir{fileChanged: make(chan fsnotify.Event)}
+func Create(paths []string, options *Options) *WatchedDir {
+	var w = WatchedDir{fileChanged: make(chan fsnotify.Event), options: options}
 	w.watcher, _ = fsnotify.NewWatcher()
 	for _, p := range paths {
 		w.walkAndAddAll(p, false)
@@ -60,18 +124,33 @@ func isHidden(name string) bool {
 
 func (w *WatchedDir) watchEvents() {
 	for evt := range w.watcher.Events {
-		if w.includeHidden == false && isHidden(evt.Name) {
+		if w.options.IncludeHidden == false && isHidden(evt.Name) {
 			continue
 		}
-		w.fileChanged <- evt
+		// Sometimes duplicate events are emitted, e.g.
+		// 1: REMOVE ./a
+		// 2: REMOVE a
+		// Let's just always remove leading './'
+		if len(evt.Name) > 2 && evt.Name[:2] == "./" {
+			evt.Name = evt.Name[2:]
+		}
+		if evt.Op.Has(fsnotify.Create) && isDir(evt.Name) {
+			go func() {
+				// This is for the edge case where e.g. a hierarchy is created like `mkdir -p a/b/c`, and b/c appears before the watcher has started watching `a`
+				time.Sleep(time.Millisecond * 100)
+				w.walkAndAddAll(evt.Name, true)
+			}()
+		}
+		if w.options.EventMask.Has(evt.Op) {
+			w.fileChanged <- evt
+		}
 	}
 }
 
 type WatchedDir struct {
-	isIndexed     bool
-	watcher       *fsnotify.Watcher
-	fileChanged   chan fsnotify.Event
-	includeHidden bool
+	watcher     *fsnotify.Watcher
+	fileChanged chan fsnotify.Event
+	options     *Options
 }
 
 func Sanitize(s string) string {
@@ -90,50 +169,74 @@ func isDir(item string) bool {
 	return stat.IsDir()
 }
 
-func WatchItems(items []string, includeHidden bool, onChange func(op fsnotify.Op, path string)) {
-	watcher := Create(items)
-	watcher.includeHidden = includeHidden
-	go watcher.watchEvents()
+func concurrentDispatcher(w *WatchedDir) {
+	for evt := range w.fileChanged {
+		go w.options.Handler(evt.Op, evt.Name, context.Background())
+	}
+}
 
-	watchedMap := make(map[string]map[fsnotify.Op]time.Time)
+func killDispatcher(w *WatchedDir) {
+	ctx, cancel := context.WithCancel(context.Background())
+	for evt := range w.fileChanged {
+		cancel()
+		ctx, cancel = context.WithCancel(context.Background())
+		go w.options.Handler(evt.Op, evt.Name, ctx)
+	}
+	cancel()
+}
 
-	for evt := range watcher.fileChanged {
-		// Sometimes duplicate events are emitted, e.g.
-		// 1: REMOVE ./a
-		// 2: REMOVE a
-		// Let's just always remove leading './'
-		if len(evt.Name) > 2 && evt.Name[:2] == "./" {
-			evt.Name = evt.Name[2:]
+func queueDispatcher(w *WatchedDir, n int) {
+	ch := make(chan fsnotify.Event, n)
+	go func() {
+		for evt := range w.fileChanged {
+			select {
+			// If the buffer is not full, add the new event
+			case ch <- evt:
+				// If the buffer is full, evict the oldest event and add the new one
+			default:
+				<-ch
+				ch <- evt
+			}
 		}
-		var map1 map[fsnotify.Op]time.Time
-		var t time.Time
-		var ok bool
+	}()
 
-		if map1, ok = watchedMap[evt.Name]; !ok {
-			map1 = make(map[fsnotify.Op]time.Time)
-			watchedMap[evt.Name] = map1
-		}
-		if t, ok = map1[evt.Op]; !ok {
-			t = time.Now()
-			map1[evt.Op] = t
-		}
+	for evt := range ch {
+		w.options.Handler(evt.Op, evt.Name, context.Background())
+	}
+}
 
-		elapsed := time.Now().Sub(t)
-		// Don't propagate an event if an identical one was propagated less than 10ms ago
-		if ok && elapsed < time.Millisecond*1000 {
-			continue
-		}
-
-		map1[evt.Op] = time.Now()
-
-		if evt.Op.Has(fsnotify.Create) && isDir(evt.Name) {
+func blockingDispatcher(w *WatchedDir) {
+	mut := sync.Mutex{}
+	for evt := range w.fileChanged {
+		if mut.TryLock() {
+			e := evt
 			go func() {
-				// This is for the edge case where e.g. a hierarchy is created like `mkdir -p a/b/c`, and b/c appears before the watcher has started watching `a`
-				time.Sleep(time.Millisecond * 100)
-				watcher.walkAndAddAll(evt.Name, true)
+				w.options.Handler(e.Op, e.Name, context.Background())
+				mut.Unlock()
 			}()
 		}
+	}
+}
 
-		onChange(evt.Op, evt.Name)
+func WatchItems(items []string, options *Options) {
+	if options == nil {
+		panic("No options set.")
+	}
+	if options.Handler == nil {
+		panic("No handler set.")
+	}
+
+	watcher := Create(items, options)
+	go watcher.watchEvents()
+
+	switch options.Mode {
+	case Concurrent:
+		concurrentDispatcher(watcher)
+	case Kill:
+		killDispatcher(watcher)
+	case Block:
+		blockingDispatcher(watcher)
+	case Queue:
+		queueDispatcher(watcher, options.QueueSize)
 	}
 }
